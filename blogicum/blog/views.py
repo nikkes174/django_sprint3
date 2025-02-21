5979382153b5d6de2f95827fa56f49f260cc2047
nikkes174 <i.suldin@icloud.com>
2025-02-21 13:31:30 +0300

    иправлены ошибки после тестов

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from unicodedata import category
+
 from .models import Category, Post
 
 
 def index(request):
     template = 'blog/index.html'
-    context = {
-        'posts': Post.objects.filter(
+    post_list = Post.objects.filter(
             pub_date__lte=timezone.now,
             is_published=True,
             category__is_published=True
         ).order_by('pub_date')[:5]
-    }
+    context = {'posts': post_list}
     return render(request, template, context)
 
 
 def category_posts(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug)
     template = 'blog/category.html'
-    posts = Post.objects.filter(
-        category=category,
+    category = get_object_or_404(Category, is_published=True,
+                                 slug=category_slug)
+    posts_list = category.posts.filter(
         is_published=True,
         pub_date__lte=timezone.now
     ).order.by('pub_date')
     context = {
         'category': category,
-        'posts': posts
+        'posts': posts_list
     }
     return render(request, template, context)
 
@@ -38,6 +39,5 @@ def post_detail(request, id):
         category__is_published=True,
         id=id
     )
-
     context = {'post': post}
     return render(request, template, context)
